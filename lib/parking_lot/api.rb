adb1ce72167612cb0af16951390e9357d6fb3fe5
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2017-10-11 00:04:37 -0300

    action scaffolds

--- lib/parking_lot/api.rb
+++ lib/parking_lot/api.rb
@@ -15,13 +15,13 @@ module ParkingLot
     put '/parking/:plate/out' do
       leave_service = ParkingLot.leave(params)
       leave_service.call
-      render leave_service.parking, serializer: :parking
+      render leave_service.parking, serializer: :leave
     end
 
     put '/parking/:plate/pay' do
       pay_service = ParkingLot.pay(params)
       pay_service.call
-      render pay_service.parking, serializer: :parking
+      render pay_service.parking, serializer: :pay
     end
 
     get '/parking/:plate' do
